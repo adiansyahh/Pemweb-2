d90206d1fd94c82815ed0990c15c72eb0a22e469
adiansyahh <115514506+adiansyahh@users.noreply.github.com>
2023-02-28 17:12:34 +0700

    Add files via upload

---
+++ week1/test.php
@@ -0,0 +1,16 @@
+<?php
+// ============================================================
+// definisikan variable
+$nama = 'Restu Pradeva Adiansyah';
+$umur = 20;
+$berat = 66;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. ' Tahun ';
+echo '<br/>Berat : ' .$berat. ' kg ';
+
+echo "<hr>";
+
+echo "<br/> Hello $nama ApaKabar?";
+echo "<br/> Hello Kabar saya baik";
+?>
